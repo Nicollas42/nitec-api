2d7f8d2d70a088c977e1476cffd56da500a2d3cb
Nicollas Lima 42 <mistrilepvp@gmail.com>
2026-03-08 03:32:44 -0300

    ajusta

--- database/migrations/tenant/2026_03_08_010227_remove_pin_pdv_from_users_table.php
+++ database/migrations/tenant/2026_03_08_010227_remove_pin_pdv_from_users_table.php
@@ -1,22 +1,24 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('pin_pdv');
-        });
+        if (Schema::hasColumn('users', 'pin_pdv')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('pin_pdv');
+            });
+        }
     }
 
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('pin_pdv', 4)->nullable();
+            $table->string('pin_pdv')->nullable();
         });
     }
 };
